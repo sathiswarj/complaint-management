a907de348169b63501056e73eae3e6d446adf45c
sathiswar <90893542+sathiswarj@users.noreply.github.com>
2024-09-10 14:02:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ const SECRET_KEY = 'secret123';
 const ADMIN_USERNAME = "admin";
 const ADMIN_PASSWORD = "admin";
 
-app.use(cors());
+app.use(cors({
+  origin: [""],
+  methods: ["GET","POST","PUT"],
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/user')

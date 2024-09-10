691ab25b6c207dc63833187a6bc4841fb9274950
sathiswar <90893542+sathiswarj@users.noreply.github.com>
2024-09-10 14:27:40 +0530

    Update Login.jsx

--- ticket/src/User/Login.jsx
+++ ticket/src/User/Login.jsx
@@ -16,6 +16,7 @@ const Login = () => {
         headers: {
           'Content-Type': 'application/json',
         },
+         credentials: 'include',
         body: JSON.stringify({ mail, password }),
       });
 
